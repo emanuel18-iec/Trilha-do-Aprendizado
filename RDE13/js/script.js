f47a5fda46429e1d662c1a098483b725c3f91d9c
emanuel18-iec <gabriel.emanuel.michalski@gmail.com>
2021-12-25 02:22:06 -0300

    versão 8.0

---
+++ RDE13/js/script.js
@@ -0,0 +1,10 @@
+var verif = false;
+while(verif == false){
+    var CdgId = prompt ("Insira o código de identificação deste site:");
+    if (CdgId == "RDE13"){
+        verif = true
+    } else {
+        verif = false
+    }
+}
+
