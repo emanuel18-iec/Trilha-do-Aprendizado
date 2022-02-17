f57a3b96d620623290f83d324af4018d641625a6
emanuel18-iec <gabriel.emanuel.michalski@gmail.com>
2022-02-17 16:30:35 -0300

    versão 10.4

---
+++ RDE15/arquivosExtra/js/Código1Aula_rde15.js
@@ -0,0 +1,17 @@
+
+
+function somar(){
+    var n1, n2, res
+
+    n1 = parseFloat(document.getElementById("n1").value)
+    n2 = parseFloat(document.getElementById("n2").value)
+
+    res = n1 + n2
+
+    document.getElementById("resultado").innerHTML = `O resultado é ${res}`
+}
+
+
+
+
+
